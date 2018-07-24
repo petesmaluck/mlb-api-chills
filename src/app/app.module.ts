c250b85460e257dd6398c18a8b86fb068f086304
Pete Smaluck <petesmaluck@petes-mbp.lan>
2018-07-24 10:06:14 -0400

    Revert "Add services to app file"
    This reverts commit a7bed009f32d5b5c7995fb8f898048897622cc7d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
 
-import { HttpDataService } from '../app/services/mlb/http-data.service';
-import { GameStatsService } from '../app/services/mlb/game-stats.service';
-import { TeamsService } from '../app/services/teams.service';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
-  providers: [HttpDataService, GameStatsService, TeamsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
