a7bed009f32d5b5c7995fb8f898048897622cc7d
Pete Smaluck <petesmaluck@petes-mbp.lan>
2018-07-24 09:49:35 -0400

    Add services to app file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+import { HttpDataService } from '../app/services/mlb/http-data.service';
+import { GameStatsService } from '../app/services/mlb/game-stats.service';
+import { TeamsService } from '../app/services/teams.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpDataService, GameStatsService, TeamsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
