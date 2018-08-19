6cf5d3ab445a8b70732473dc9911c8aea7365572
Pete Smaluck <petesmaluck@gmail.com>
2018-08-19 15:11:25 -0700

    Initial setup of services (#3)
    **Context**
    Setting up the initial api calls to the MLB API that will be used throughout the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+// Components
 import { AppComponent } from './app.component';
+
+//Services
+import { HttpDataService } from '../app/services/util/http-data.service';
+import { GameStatsService } from '../app/services/mlb/game-stats.service';
+import { TeamsService } from '../app/services/mlb/teams.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpDataService,
+    GameStatsService,
+    TeamsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
